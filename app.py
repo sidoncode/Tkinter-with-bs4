a4cc8fbca1a5b849067c4146ffb6b576258847a8
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2019-08-27 00:41:19 +0530

    error in I/O csv file

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-from bs4 import BeautifulSoup
-import request
-def __main__():
-    
+import csv
+with open("NIFTY50all28-08-2018-TO-27-08-2019.csv","rb") as NIFTY50:
+    NIFTY50reader = csv.reader(NIFTY50)
+    x = csv.reader(NIFTY50,delimiter=',',quotechar='|') #transformed from stackoverflow
+    NIFTY50list = []
+for row in x:
+    print(x)
+    '''if len(row) != 0:
+        NIFTY50list = NIFTY50list + [row]
+NIFTY50.close()
+print(NIFTY50)
+with open("NIFTY50all28-08-2018-TO-27-08-2019.csv","r") as NIFTY50:
+    x = csv.reader(NIFTY50,delimiter=',',quotechar='|')
+    pe = (input("Enter the pe value for the csv file"))
+    NIFTY50reader = csv.reader(NIFTY50)
+for row in x: 
+    print(row)
+'''
